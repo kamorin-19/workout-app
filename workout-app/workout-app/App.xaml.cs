cb6c22ea68b95f01c697ea6b73d3c8f556c400c3
rinyou-19 <long_drive19@yahoo.co.jp>
2024-04-08 22:48:07 +0900

    create a project

---
+++ workout-app/workout-app/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace workout_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
